442758cc33f2f75f2a742174ccd85b7a60ff0292
Byunk <clearman001@gmail.com>
2023-05-16 17:12:08 +0900

    feat: add create organization api

--- src/app/routes/application.router.ts
+++ src/app/routes/application.router.ts
@@ -1,7 +1,9 @@
 import express, { Router } from 'express';
 import { usersRouter } from './users.router';
+import { organizationsRouter } from './organization.router';
 
 const router: Router = express.Router();
 router.use('/users', usersRouter);
+router.use('/organization', organizationsRouter);
 
 export const applicationRouter: Router = router;
