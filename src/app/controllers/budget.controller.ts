2e64e53ce594e8bff1f40525444677f3d37cd668
youngmin yoo <yym3055@gmail.com>
2023-05-21 14:09:50 +0900

    fix error on  connect_to_db() function with errormessage: self._addDefaultMeta error. logger.info -> console.info

--- src/app/controllers/budget.controller.ts
+++ src/app/controllers/budget.controller.ts
@@ -6,11 +6,11 @@ export async function register_expense(req: Request, res: Response, next: NextFu
         
         const expense = await Budget.create({
             organization: req.params.org_id,
-            item: req.params.item_id, // item id 어떻게 부여받고 전달 받는지 규명 필요
+            item: req.body.item_id, // Request body에 item 0bject id를 넣어 주는 것으로 가정
             budget: req.body.budget,
             remarks: req.body.remarks,
         });
-        res.json(expense); // DB Expense collections에 등록 필요?
+        res.json(expense); // DB Expense collections에 등록 필요
     } catch (err) {
         next(err);
     }
