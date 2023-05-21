8bd9ce30071be4f735ffa406de5f30c07f6669c0
younjihoon <jihoon9835@gmail.com>
2023-05-21 21:56:38 +0900

    create no work

--- src/app/controllers/item.controller.ts
+++ src/app/controllers/item.controller.ts
@@ -1,12 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 import { Item } from '../schemas/item';
 
+
 export async function create(req: Request, res: Response, next: NextFunction) {
   try {
     let newItem;
     if(req.body.item){
-    const { organization, year, item, item_code} = req.body;
-    newItem = new Item( { organization, year, item, item_code});
+    const { organization, year, item} = req.body;
+    newItem = new Item( { organization, year, item});
     }
     if(!req.body.item) {
     const { organization, year, sub_item, detail_item} = req.body;
@@ -24,25 +25,29 @@ export async function modify(req: Request, res: Response, next: NextFunction) {
     const updateData = req.body;
   
     try {
-      const user = await Item.findByIdAndUpdate(id, updateData, { new: true });
-      res.json(user);
-    } catch (error) {
-      next(error);
-    }
-}
-
-export async function deleteItem(req: Request, res: Response, next: NextFunction) {
-    try {
-      const item;
+      const item = await Item.findByIdAndUpdate(id, updateData, { new: true });
       res.json(item);
     } catch (error) {
       next(error);
     }
 }
 
-export async function getByYear(req: Request, res: Response, next: NextFunction) {
+export async function deleteItem(req: Request, res: Response, next: NextFunction) {
+    const itemID = req.params.id;
     try {
-      const item;
+      const item = await Item.findByIdAndDelete(itemID);
+      res.json({ message: 'item successfully deleted', item });
+    } catch (error) {
+      next(error);
+    }
+}
+
+export async function getByYear(req: Request, res: Response, next: NextFunction) {
+    const itemYear = req.params.year;
+
+    try {
+      const item = await Item.findOne({year:itemYear});
+  //    logger.info(item);
       res.json(item);
     } catch (error) {
       next(error);
@@ -50,8 +55,11 @@ export async function getByYear(req: Request, res: Response, next: NextFunction)
 }
 
 export async function getByOrganization(req: Request, res: Response, next: NextFunction) {
+    const itemOrganization = req.params.Organization;
+
     try {
-      const item;
+      const item = await Item.findOne({organization:itemOrganization});
+   //   logger.info(item);
       res.json(item);
     } catch (error) {
       next(error);
--- src/app/routes/item.router.ts
+++ src/app/routes/item.router.ts
@@ -5,6 +5,6 @@ const router: Router = express.Router();
 router.route('/createItem').post(ItemController.create);
 router.route('/modifyItem/:id').put(ItemController.modify);
 router.route('/deleteItem/:id').delete(ItemController.deleteItem);
-router.route('/getItem/:year').get(ItemController.getByYear);
-router.route('/getItem/:organization').get(ItemController.getByOrganization)
+router.route('/getItemYear/:year').get(ItemController.getByYear);
+router.route('/getItemOrgan/:organization').get(ItemController.getByOrganization)
 export const itemRouter: Router = router;
