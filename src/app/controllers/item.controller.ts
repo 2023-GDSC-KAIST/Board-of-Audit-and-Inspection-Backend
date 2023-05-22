1609cce4c15c43e595a2bb006cd04e7cf8189c3e
youngmin yoo <yym3055@gmail.com>
2023-05-22 20:16:43 +0900

    add item controller router by jihun

---
+++ src/app/controllers/item.controller.ts
@@ -0,0 +1,64 @@
+import { NextFunction, Request, Response } from 'express';
+import { Item } from '../schemas/item';
+
+
+export async function create(req: Request, res: Response, next: NextFunction) {
+  try {
+    const organization =req.body.organization;
+    const year= req.body.year;
+    const item = req.body.item;
+    const sub_item= req.body.sub_item;
+    const detail_item= req.body.detail_item;
+    const newItem = new Item( { organization, year, item, sub_item, detail_item});
+
+    res.json(newItem);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function modify(req: Request, res: Response, next: NextFunction) {
+    const { id } = req.params;
+    const updateData = req.body;
+  
+    try {
+      const item = await Item.findByIdAndUpdate(id, updateData, { new: true });
+      res.json(item);
+    } catch (error) {
+      next(error);
+    }
+}
+
+export async function deleteItem(req: Request, res: Response, next: NextFunction) {
+    const itemID = req.params.id;
+    try {
+      const item = await Item.findByIdAndDelete(itemID);
+      res.json({ message: 'item successfully deleted', item });
+    } catch (error) {
+      next(error);
+    }
+}
+
+export async function getByYear(req: Request, res: Response, next: NextFunction) {
+    const itemYear = req.params.year;
+
+    try {
+      const item = await Item.findOne({year:itemYear});
+  //    logger.info(item);
+      res.json(item);
+    } catch (error) {
+      next(error);
+    }
+}
+
+export async function getByOrganization(req: Request, res: Response, next: NextFunction) {
+    const itemOrganization = req.params.Organization;
+
+    try {
+      const item = await Item.findOne({organization:itemOrganization});
+   //   logger.info(item);
+      res.json(item);
+    } catch (error) {
+      next(error);
+    }
+}
--- src/app/routes/item.router.ts
+++ src/app/routes/item.router.ts
@@ -1,5 +1,17 @@
 import express, { Router } from 'express';
+import { ItemController } from '../controllers';
 
 const router: Router = express.Router();
+router
+  .route('/')
+  .post(ItemController.create);
 
+router
+  .route('/:id')
+  .patch(ItemController.modify)
+
+  .delete(ItemController.deleteItem);
+
+router.route('/getItemYear/:year').get(ItemController.getByYear);
+router.route('/getItemOrgan/:organization').get(ItemController.getByOrganization)
 export const itemRouter: Router = router;
