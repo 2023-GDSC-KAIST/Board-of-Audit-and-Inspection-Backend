39377b77a35336f8b4c2e4b2f198f309e8f70a98
Byunk <clearman001@gmail.com>
2023-05-23 16:57:20 +0900

    Revert "feat: add controllers and routers"
    This reverts commit 9b7d98bbab62cba0e75656fff061be8eb274eaa2.

--- src/app/routes/application.router.ts
+++ src/app/routes/application.router.ts
@@ -1,13 +1,7 @@
 import express, { Router } from 'express';
 import { organizationRouter } from './organization.router';
-import { budgetRouter } from './budget.router';
-import { itemRouter } from './item.router';
-import { transactionRouter } from './transaction.router';
 
 const router: Router = express.Router();
 router.use('/organization', organizationRouter);
-router.use('/budget', budgetRouter);
-router.use('/item', itemRouter);
-router.use('/transaction', transactionRouter);
 
 export const applicationRouter: Router = router;
