407a2a79411d49a72d590908fbc4954a0df6e154
Byunk <clearman001@gmail.com>
2023-05-25 00:54:42 +0900

    fix: unhandled conflict

--- src/app/routes/item.router.ts
+++ src/app/routes/item.router.ts
@@ -2,21 +2,8 @@ import express, { Router } from 'express';
 import { ItemController } from '../controllers';
 
 const router: Router = express.Router();
-router
-  .route('/')
-  .post(ItemController.create);
 
 router
-<<<<<<< HEAD
-  .route('/:id')
-  .patch(ItemController.modify)
-
-  .delete(ItemController.deleteItem);
-
-router.route('/getItemYear/:year').get(ItemController.getByYear);
-router.route('/getItemOrgan/:organization').get(ItemController.getByOrganization)
-export const itemRouter: Router = router;
-=======
   .route('/:organization/:year')
   .get(ItemController.listItems)
   .post(ItemController.createItem);
@@ -27,4 +14,3 @@ router
   .delete(ItemController.deleteItem);
 
 export const itemRouter: Router = router;
->>>>>>> develop
