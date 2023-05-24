8c54591423971226524646da965d740521c76f09
Byunk <clearman001@gmail.com>
2023-05-24 22:08:15 +0900

    feat: create organization api

--- src/app/controllers/organization.controller.ts
+++ src/app/controllers/organization.controller.ts
@@ -1,11 +1,25 @@
 import { NextFunction, Request, Response } from 'express';
 import { Organization } from '../schemas/organization';
 
-export async function createOrganization(req: Request, res: Response, next: NextFunction) {
+export async function createOrganization(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
   try {
-    const organization = await Organization.create({
-      name: req.body.name,
-    });
+    // organization 중복체크 및 생성
+    const organization = await Organization.findOneAndUpdate(
+      {
+        name: req.body.name,
+      },
+      {
+        name: req.body.name,
+      },
+      {
+        upsert: true,
+        new: true,
+      },
+    );
     res.json(organization);
   } catch (error) {
     next(error);
