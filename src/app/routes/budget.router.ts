909642c2d4adaebb4a4430a834de9d2d293df739
Haeseul <jjchsjj@gmail.com>
2023-05-25 16:35:22 +0900

    Fix router address

--- src/app/routes/budget.router.ts
+++ src/app/routes/budget.router.ts
@@ -3,10 +3,10 @@ import { BudgetController } from '../controllers';
 
 const router: Router = express.Router();
 
-router
-  .route('/:id')
-  .delete(BudgetController.deleteBudget);
+router.route('/:id').delete(BudgetController.deleteBudget);
 
-router.route('/getBudget/:organization/:year/settlement').get(BudgetController.getSettlementByOrganizationAndYear);
+router
+  .route('/:organization/:year/settlement')
+  .get(BudgetController.getSettlementByOrganizationAndYear);
 
 export const budgetRouter: Router = router;
